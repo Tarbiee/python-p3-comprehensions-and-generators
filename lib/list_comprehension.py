677d2aa7cb8520cd1576013c0a34c11139563424
Tarbiee <dorcusstaby@gmail.com>
2023-11-22 23:14:19 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,23 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
+# evens = list()
+# def return_evens(num_list):
+#     for n in num_list:
+#         if n % 2 == 0:
+#             evens.append(n)
+#     return evens
+# return_evens([0, 1, 3, 5, 7, 8, 9])
+
 
 def return_evens(num_list):
-    pass
+    evens =[n for n in num_list if n%2 ==0]  
+    if not evens:
+        return[]
+    else:
+        return evens
+return_evens([0, 1, 3, 5, 7, 8, 9])
+
+    
 
 def make_exclamation(sentence_list):
-    pass
+    sentence = [n + "!" for n in sentence_list ]
+    return sentence
